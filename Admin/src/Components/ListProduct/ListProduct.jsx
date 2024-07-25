b71330ead6534793a9d663260b5aa5f1aef2e1a4
GarvVirmani <131982472+GarvVirmani@users.noreply.github.com>
2024-07-25 20:07:28 +0530

    Update ListProduct.jsx

--- Admin/src/Components/ListProduct/ListProduct.jsx
+++ Admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://ecommerce-backend-b5w1.onrender.com/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
